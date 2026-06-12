702bcd31e1f9c1c6b74e0468834d2c589483e8d4
Andriy Yevtushyn <129842532+Avionic23@users.noreply.github.com>
2026-06-12 19:53:41 +0300

    feat: add circuit breaker to the backend

--- backend/backend.go
+++ backend/backend.go
@@ -3,6 +3,7 @@ package backend
 import (
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
 type Backend struct {
@@ -10,17 +11,40 @@ type Backend struct {
 	healthy     atomic.Bool
 	weight      int
 	activeConns atomic.Int64
+	cb          CircuitBreaker
 }
 
 type BackendOptions struct {
-	Url    string
-	Weight int
+	Url                      string
+	Weight                   int
+	CircuitWindowSize        int
+	CircuitFailureThreshold  int
+	CircuitHalfOpenThreshold int
+	CircuitCooldownTimeout   time.Duration
 }
 
 func NewBackend(opts BackendOptions) *Backend {
+	if opts.CircuitWindowSize == 0 {
+		opts.CircuitWindowSize = 10
+	}
+	if opts.CircuitFailureThreshold == 0 {
+		opts.CircuitFailureThreshold = 50
+	}
+	if opts.CircuitHalfOpenThreshold == 0 {
+		opts.CircuitHalfOpenThreshold = 3
+	}
+	if opts.CircuitCooldownTimeout == 0 {
+		opts.CircuitCooldownTimeout = 30 * time.Second
+	}
 	return &Backend{
 		url:    opts.Url,
 		weight: opts.Weight,
+		cb: NewCircuitBreaker(CircuitBreakerOptions{
+			WindowSize:        opts.CircuitWindowSize,
+			FailureThreshold:  opts.CircuitFailureThreshold,
+			HalfOpenThreshold: opts.CircuitHalfOpenThreshold,
+			CooldownTimeout:   opts.CircuitCooldownTimeout,
+		}),
 	}
 }
 
@@ -50,6 +74,18 @@ func (b *Backend) DecrActiveConns() {
 
 func (b *Backend) GetActiveConns() int64 {
 	return b.activeConns.Load()
+}
+
+func (b *Backend) IsOpen() bool {
+	return b.cb.IsOpen()
+}
+
+func (b *Backend) RecordSuccess() {
+	b.cb.RecordSuccess()
+}
+
+func (b *Backend) RecordFailure() {
+	b.cb.RecordFailure()
 }
 
 type BackendPool struct {
